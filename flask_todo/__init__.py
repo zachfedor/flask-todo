c2a700ee4799123c0801604179e224c373b71f9d
zachfedor <zachfedor@gmail.com>
2019-03-27 15:28:53 -0400

    Adds psycopg adapter

--- flask_todo/__init__.py
+++ flask_todo/__init__.py
@@ -1,3 +1,5 @@
+import datetime
+
 from flask import Flask, flash, make_response, render_template, request
 
 
@@ -6,6 +8,8 @@ def create_app(test_config=None):
 
     app.config.from_mapping(
         SECRET_KEY='dev',
+        DB_NAME='flasktodo',
+        DB_USER='flasktodo_user',
     )
 
     if test_config is None:
@@ -13,37 +17,61 @@ def create_app(test_config=None):
     else:
         app.config.from_mapping(test_config)
 
+    from . import db
+    db.init_app(app)
 
     @app.route('/')
     def index():
-        # TODO: Get todos from database
-        mock_todos = [
-            { "text": "do dishes", "created_at": "2019-03-22", "completed": False },
-            { "text": "feed dog", "created_at": "2019-03-22", "completed": False },
-            { "text": "clean room", "created_at": "2019-03-22", "completed": True },
-        ]
+        
+        con = db.get_db()
+        cur = con.cursor()
+        cur.execute("SELECT * FROM items")
+        todo_results = cur.fetchall()
+        cur.close()
+
+        todos = []
+        for result in todo_results:
+            todos.append({
+                "id": result[0],
+                "text": result[1],
+                "created_at": result[2],
+                "completed": result[3],
+            })
+
 
         filter_option = request.args.get('filter')
         if filter_option == 'completed':
-            mock_todos = list(filter(lambda t: t['completed'], mock_todos))
+            todos = list(filter(lambda t: t['completed'], todos))
         elif filter_option == 'active':
-            mock_todos = list(filter(lambda t: not t['completed'], mock_todos))
+            todos = list(filter(lambda t: not t['completed'], todos))
 
         # TODO: add ability to sort items
         # sort_option = request.args.get('sort')
 
-        return render_template('index.html', todos=mock_todos, filter_option=filter_option)
+        return render_template('index.html', todos=todos, filter_option=filter_option)
 
 
     @app.route('/create', methods=["GET", "POST"])
     def create():
         if request.method == "POST":
-            # TODO: Save request.form data to database
-            # TODO: If error, flash it to the screen
             new_item = request.form["text"]
 
             if new_item:
-                print("Create Todo: ", new_item)
+                dt = datetime.datetime.now()
+                print("Creating Todo: ", new_item)
+
+                # Save to database
+                con = db.get_db()
+                cur = con.cursor()
+                cur.execute(
+                        "INSERT INTO items (text, created_at, completed) VALUES (%s, %s, %s)",
+                        (new_item, dt, False)
+                )
+                con.commit()
+                cur.close()
+
+                # TODO: If error, flash it to the screen
+
                 flash('To-Do item was added. Want to add another?', 'success')
             else:
                 flash('You need to add some text first.', 'error')
